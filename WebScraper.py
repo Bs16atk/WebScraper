70679b527bb330fc00c045b73363625f261b88db
Bs16atk <85703614+Bs16atk@users.noreply.github.com>
2021-06-25 22:30:40 +0100

    Some more changes

--- WebScraper.py
+++ WebScraper.py
@@ -1,16 +1,24 @@
+#importing a load of libraries
 from urllib.request import urlopen, urlparse, urljoin
+import re
 import requests
+from collections import Counter
+from string import punctuation
 from bs4 import BeautifulSoup
 import json
 
+#Task1
+print("Scraping Index Page")
 html = urlopen('https://www.cfcunderwriting.com/en-gb/')
 
-
+#Scrape the index page
 bs = BeautifulSoup(html, 'html.parser')
 images = bs.find_all('div',class_='img')
 scripts = bs.find_all('script')
 links = bs.find_all('link')
 morelinks = bs.find_all('a', href=True)
+
+#Task2
 
 items = []
 for image in images: 
@@ -25,6 +33,7 @@ for link in links:
     record = {"Link":link}
     items.append(str(record))
 
+#Trying to only show external resources, this mostly worked
 a = 'https://'
 for item in items:
     if a in item:
@@ -32,13 +41,32 @@ for item in items:
     else:
         items.remove(item)
 
-with open('results.json', 'w', encoding='utf-8') as write_file:
- output = json.dumps(items, indent=2)
- write_file.write(output)
-
-#Task 2
+#Task 3 to find the privacy policy by enumerating through the links in the page
 for x in morelinks:
     if "privacy" in str(x):
-        print("The Privacy Policy can be found at: " + str(x))
+        privlink = str(x)
+        print("\nThe Privacy Policy can be found at: " + str(x))
     
-    
+#Task 4 to scrape the privacy policy page and perform a word frequency count
+print("\nScraping Privacy Policy")
+r = requests.get("https://www.cfcunderwriting.com/en-gb/support/privacy-policy/")
+bs1 = BeautifulSoup(r.content, 'html.parser')
+
+text_p = (''.join(bs1.findAll(text=True))for thing in bs1.findAll('p'))
+c_p = Counter((x.rstrip(punctuation).lower() for y in text_p for x in y.split()))
+
+text_div = (''.join(bs1.findAll(text=True))for thing in bs1.findAll('div'))
+c_div = Counter((x.rstrip(punctuation).lower() for y in text_div for x in y.split()))
+
+total = c_div + c_p
+list_most_common_words = total.most_common()
+words = total.most_common(1000)
+#reformatting the word frequency count to what I think is more like JSON
+words1 = json.dumps(dict(words), indent=2)
+
+#writing all results to results.json
+with open('results.json', 'w', encoding='utf-8') as write_file:
+ output = json.dumps(items, indent=2)
+ write_file.write(output + words1)
+
+print("\nWrote to results.json")
