1aa0703d2fb0abde082ecb434d161059b4b56d2b
Bs16atk <85703614+Bs16atk@users.noreply.github.com>
2021-06-25 20:11:57 +0100

    More changes

--- WebScraper.py
+++ WebScraper.py
@@ -9,18 +9,22 @@ html = urlopen('https://www.cfcunderwriting.com/en-gb/')
 bs = BeautifulSoup(html, 'html.parser')
 images = bs.find_all('div',class_='img')
 scripts = bs.find_all('script')
+links = bs.find_all('link')
 
 items = []
 for image in images: 
     record = {"Image":image}
     items.append(str(record))
 
-items1 = []
 for script in scripts: 
     record = {"Script":script}
     items.append(str(record))
 
-a = "https://"
+for link in links: 
+    record = {"Link":link}
+    items.append(str(record))
+
+a = 'https://'
 for item in items:
     if a in item:
         continue
@@ -28,6 +32,12 @@ for item in items:
         items.remove(item)
 
 with open('results.json', 'w', encoding='utf-8') as write_file:
- output = json.dumps(items, indent=0)
+ output = json.dumps(items, indent=2)
  write_file.write(output)
+
+b = 'privacy'
+for link in links:
+    if b in link:
+        print(link)
+    
     
