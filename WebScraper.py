7738edd508d2054bcbf6713d61e69712a9c10f6a
Bs16atk <85703614+Bs16atk@users.noreply.github.com>
2021-06-25 22:39:20 +0100

    More aesthetic touches

--- WebScraper.py
+++ WebScraper.py
@@ -18,7 +18,6 @@ links = bs.find_all('link')
 morelinks = bs.find_all('a', href=True)
 
 #Task2
-
 items = []
 for image in images: 
     record = {"Image":image}
@@ -31,7 +30,6 @@ for script in scripts:
 for link in links: 
     record = {"Link":link}
     items.append(str(record))
-
 #Trying to only show external resources, this mostly worked
 a = 'https://'
 for item in items:
@@ -46,17 +44,13 @@ with open('resources.json', 'w', encoding='utf-8') as write_file:
  write_file.write(output)
  print("\nWrote to resources.json")
 
-
 #Task 3 to find the privacy policy by enumerating through the links in the page
-
 for x in morelinks:
     if "privacy" in str(x):
         privlink = str(x)
         print("\nThe Privacy Policy can be found at: " + str(x))
 
-  
 #Task 4 to scrape the privacy policy page and perform a word frequency count
-
 print("\nScraping Privacy Policy")
 r = requests.get("https://www.cfcunderwriting.com/en-gb/support/privacy-policy/")
 bs1 = BeautifulSoup(r.content, 'html.parser')
